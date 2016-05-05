1a768cc2ff39b42fe3d2ac42cb020fd1c769375d
Svetlozar Kirkov <svetlozark@gmail.com>
2016-05-05 13:11:35 +0300

    upd csharpadv-h1

---
+++ CSharp-Advanced/01-Arrays/04-MaximalSequence/App.cs
@@ -0,0 +1,39 @@
+﻿namespace _04_MaximalSequence
+{
+    using System;
+
+    internal class App
+    {
+        private static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var array = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            var maxLength = 1;
+            for (var i = 0; i < n; i++)
+            {
+                var currentNum = array[i];
+                var currentLength = 1;
+
+                for (var j = i + 1; j < n; j++)
+                {
+                    if (!array[j].Equals(currentNum))
+                    {
+                        break;
+                    }
+
+                    currentLength++;
+                }
+
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                }
+            }
+            Console.WriteLine(maxLength);
+        }
+    }
+}
---
+++ CSharp-Advanced/01-Arrays/05-MaximalIncreasingSequence/App.cs
@@ -0,0 +1,50 @@
+﻿namespace _05_MaximalIncreasingSequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class App
+    {
+        // TODO: Fix
+        private static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var array = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            var result = GetMaximalIncreasingSequence(array);
+            Console.WriteLine(result);
+        }
+
+        private static int GetMaximalIncreasingSequence(IReadOnlyList<int> array)
+        {
+            var maxLength = 1;
+
+            for (var i = 0; i < array.Count; i++)
+            {
+                var currentNum = array[i];
+                var currentLength = 1;
+
+                for (var j = i + 1; j < array.Count; j++)
+                {
+                    if (array[j] != currentNum + 1)
+                    {
+                        break;
+                    }
+
+                    currentLength++;
+                    currentNum = array[j];
+                }
+
+                if (currentLength > maxLength)
+                {
+                    maxLength = currentLength;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
