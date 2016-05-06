dca674a84c4272e5c2b313cdaf191478f917fc69
Svetlozar Kirkov <svetlozark@gmail.com>
2016-05-06 18:05:55 +0300

    flush small changes

--- CSharp-Advanced/01-Arrays/03-CompareCharArrays/App.cs
+++ CSharp-Advanced/01-Arrays/03-CompareCharArrays/App.cs
@@ -1,42 +1,25 @@
 ﻿namespace _03_CompareCharArrays
 {
     using System;
-    using System.Collections.Generic;
 
     internal class App
     {
         private static void Main()
         {
-            var firstArray = Console.ReadLine().ToCharArray();
-            var secondArray = Console.ReadLine().ToCharArray();
-            var comparer = new LexicographicCharArrayComparer();
-            var result = comparer.Compare(firstArray, secondArray);
-            if (result < 0)
-            {
-                Console.WriteLine("<");
-            }
-            else if (result == 0)
+            var firstArray = Console.ReadLine();
+            var secondArray = Console.ReadLine();
+            var result = string.CompareOrdinal(firstArray, secondArray);
+            if (result == 0)
             {
                 Console.WriteLine("=");
             }
-            else
+            else if (result < 0)
+            {
+                Console.WriteLine("<");
+            }
+            else if (result > 0)
             {
                 Console.WriteLine(">");
-            }
-        }
-
-        private class LexicographicCharArrayComparer : Comparer<char[]>
-        {
-            /// <exception cref="OverflowException">The array is multidimensional and contains more than <see cref="F:System.Int32.MaxValue" /> elements.</exception>
-            public override int Compare(char[] x, char[] y)
-            {
-                if (x == null || y == null)
-                {
-                    return Default.Compare(x, y);
-                }
-
-                var lengthComp = x.Length.CompareTo(y.Length);
-                return lengthComp != 0 ? lengthComp : StringComparer.Ordinal.Compare(new string(x), new string(y));
             }
         }
     }
---
+++ CSharp-Advanced/01-Arrays/06-MaximalKSum/App.cs
@@ -0,0 +1,19 @@
+﻿namespace _06_MaximalKSum
+{
+    using System;
+
+    internal class App
+    {
+        private static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+            var array = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
